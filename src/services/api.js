2af83d5cf63f35f388561f761c1964e9f7109bc3
Uriel Summonte <seti137@gmail.com>
2025-05-03 22:53:11 -0300

    corregido cors

--- src/services/api.js
+++ src/services/api.js
@@ -2,8 +2,7 @@ import axios from "axios"
 console.log("API URL:", import.meta.env.VITE_API_URL);
 
 const api = axios.create({
-  //baseURL: import.meta.env.VITE_API_URL,
-  baseURL: "https://moviesflix-backend.onrender.com/api",
+  baseURL: import.meta.env.VITE_API_URL,
   headers: {
     "Content-Type": "application/json",
   },
